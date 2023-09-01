466a9f7ffe4647228f7115733dd473547432d387
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-09-01 11:59:32 +0530

    CSV Json operations

---
+++ CSVDemo/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSVDemo
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+    }
+}
